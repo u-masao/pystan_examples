7ced05610c0ba40d15e5bc4b7b5b792dd6fad2f2
u-masao <4973920+u-masao@users.noreply.github.com>
2022-05-05 15:53:52 +0900

    [update] add app

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,6 +1,8 @@
+import urllib.request
 from decimal import ROUND_HALF_UP, Decimal
 
 import pandas as pd
+import rdata
 import stan
 import streamlit as st
 
@@ -36,7 +38,7 @@ def download_and_visualize():
 
 
 def app_0_0_getting_started():
-    schools_code = """
+    stan_code = """
     data {
       int<lower=0> J;         // number of schools
       real y[J];              // estimated treatment effects
@@ -56,28 +58,93 @@ def app_0_0_getting_started():
     }
     """
 
-    schools_data = {
+    stan_data = {
         "J": 8,
         "y": [28, 8, -3, 7, -1, 1, 18, 12],
         "sigma": [15, 10, 16, 11, 9, 11, 10, 18],
     }
 
+    simulation(stan_code, stan_data)
+
+
+def simulation(stan_code, stan_data, num_chains=4, num_samples=1000, seed=1234):
     with st.spinner("building stan code"):
-        posterior = stan.build(schools_code, data=schools_data)
+        model = stan.build(stan_code, data=stan_data, random_seed=seed)
     with st.spinner("sampling now"):
-        fit = posterior.sample(num_chains=4, num_samples=1000)
-    eta = fit["eta"]  # array with shape (8, 4000)
-    df = fit.to_frame()  # pandas `DataFrame, requires pandas
-
+        fit = model.sample(num_chains=num_chains, num_samples=num_samples)
+    df = fit.to_frame()
     st.dataframe(df)
-    st.table(eta.T)
+    st.dataframe(df.describe().T)
+    return fit
 
-    st.line_chart(eta.T)
+
+def load_rdata(url):
+    with urllib.request.urlopen(url) as fo:
+        parsed = rdata.parser.parse_data(fo.read())
+    return rdata.conversion.convert(parsed)
+
+
+def app_tsbook_10_1():
+    dataset = load_rdata(
+        "https://github.com/hagijyun/tsbook/raw/master/"
+        "ArtifitialLocalLevelModel.RData"
+    )
+
+    print(type(dataset))
+
+    for key, value in dataset.items():
+        print("====", key, type(value))
+        if type(value) == dict:
+            for k, v in value.items():
+                print(k, v)
+        print(value)
+
+    stan_code = """
+    data{
+        int<lower=1> t_max;
+        vector[t_max] y;
+
+        cov_matrix[1] W;
+        cov_matrix[1] V;
+        real m0;
+        cov_matrix[1] C0;
+    }
+
+    parameters{
+        real x0;
+        vector[t_max] x;
+    }
+
+    model{
+        for (t in 1:t_max){
+            y[t] ~ normal(x[t], sqrt(V[1, 1]));
+        }
+        x0 ~ normal(m0, sqrt(C0[1, 1]));
+        x[1] ~ normal(x0, sqrt(W[1, 1]));
+        for (t in 2:t_max){
+            x[t] ~ normal(x[t-1], sqrt(W[1, 1]));
+        }
+    }
+    """
+
+    stan_data = {}
+    stan_data["t_max"] = int(dataset["t_max"][0])
+    stan_data["y"] = dataset["y"].values
+    stan_data["W"] = dataset["mod"]["W"]
+    stan_data["V"] = dataset["mod"]["V"]
+    stan_data["m0"] = dataset["mod"]["m0"][0]
+    stan_data["C0"] = dataset["mod"]["C0"]
+
+    print(stan_data)
+    simulation(stan_code, stan_data)
 
 
 def main():
-    apps = {"0_0_getting_started": app_0_0_getting_started}
-    app_key = st.sidebar.selectbox("app", apps.keys())
+    apps = {
+        "0_0_getting_started": app_0_0_getting_started,
+        "tsbook_10_1": app_tsbook_10_1,
+    }
+    app_key = st.sidebar.selectbox("app", sorted(apps.keys()))
     apps[app_key]()
 
 
